f882a509939d9805b5eff60cef3d67d068964355
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-16 15:35:05 +0100

    Initialize text view with lateinit

--- app/src/main/java/com/amqo/habittrainer/MainActivity.kt
+++ app/src/main/java/com/amqo/habittrainer/MainActivity.kt
@@ -2,11 +2,18 @@ package com.amqo.habittrainer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit means this variable does not need to be init at this point
+    private lateinit var tvDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvDescription = findViewById(R.id.tv_description)
+        tvDescription.text = getString(R.string.water_description)
     }
 }
