905b7f1d6fc7889bc16dd9928bc69cd0b26fdd2b
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-16 15:36:10 +0100

    Get advantage of kotlin extensions to avoid findViewbyId

--- app/src/main/java/com/amqo/habittrainer/MainActivity.kt
+++ app/src/main/java/com/amqo/habittrainer/MainActivity.kt
@@ -2,18 +2,14 @@ package com.amqo.habittrainer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    // lateinit means this variable does not need to be init at this point
-    private lateinit var tvDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        tvDescription = findViewById(R.id.tv_description)
-        tvDescription.text = getString(R.string.water_description)
+        
+        tv_description.text = getString(R.string.water_description)
     }
 }
