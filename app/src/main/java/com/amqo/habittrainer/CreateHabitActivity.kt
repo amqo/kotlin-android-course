61f60b20024575072d385a55eea4985814bdeb9f
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-17 11:57:33 +0100

    Added menu to create new habit, and the empty activity to show it

---
+++ app/src/main/java/com/amqo/habittrainer/CreateHabitActivity.kt
@@ -0,0 +1,12 @@
+package com.amqo.habittrainer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CreateHabitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_habit)
+    }
+}
--- app/src/main/java/com/amqo/habittrainer/MainActivity.kt
+++ app/src/main/java/com/amqo/habittrainer/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.amqo.habittrainer
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Adapter -> defines data
-        // RecyclerView -> implement 3 methods
         rv.setHasFixedSize(true)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = HabitsAdapter(getSampleHabits())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_habit) {
+            startActivity(Intent(this, CreateHabitActivity::class.java))
+        }
+        return true
     }
 }
