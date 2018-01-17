498ff65e060c5b6d0245a90991e895d22249c051
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-17 16:22:15 +0100

    Refactor chooser to get result

--- app/src/main/java/com/amqo/habittrainer/CreateHabitActivity.kt
+++ app/src/main/java/com/amqo/habittrainer/CreateHabitActivity.kt
@@ -1,18 +1,50 @@
 package com.amqo.habittrainer
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_habit.*
+import java.io.IOException
 
 class CreateHabitActivity : AppCompatActivity() {
 
     private val TAG = CreateHabitActivity::class.java.simpleName
 
+    private val CHOOSE_IMAGE_REQUEST = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_habit)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CHOOSE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
+                && data != null && data.data != null) {
+            Log.d(TAG, "An image was chosen by the user")
+
+            val bitmap = tryReadBitmap(data.data)
+            bitmap?.let {
+                iv_image.setImageBitmap(bitmap)
+                Log.d(TAG, "Read image bitmap and update image view")
+            }
+        }
+    }
+
+    private fun tryReadBitmap(data: Uri): Bitmap? {
+        return try {
+            MediaStore.Images.Media.getBitmap(contentResolver, data)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
 
     fun chooseImage(v: View) {
@@ -20,7 +52,8 @@ class CreateHabitActivity : AppCompatActivity() {
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
 
-        startActivity(Intent.createChooser(intent, "Choose image for habit"))
+        val chooser = Intent.createChooser(intent, "Choose image for habit")
+        startActivityForResult(chooser, CHOOSE_IMAGE_REQUEST)
 
         Log.d(TAG, "Intent to choose image sent...")
     }
