6d0ccdd92431c3732fe0d5495967f32ab3d6e865
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-18 13:07:39 +0100

    Refactor code to use extension functions when reading Habits

--- app/src/main/java/com/amqo/habittrainer/MainActivity.kt
+++ app/src/main/java/com/amqo/habittrainer/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
+import com.amqo.habittrainer.persistence.HabitDbTable
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        rv.setHasFixedSize(true)
-        rv.layoutManager = LinearLayoutManager(this)
-//        rv.adapter = HabitsAdapter(getSampleHabits())
+    override fun onPostResume() {
+        super.onPostResume()
+        initHabitsList()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,5 +35,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun switchTo(c: Class<*>) {
         startActivity(Intent(this, c))
+    }
+
+    private fun initHabitsList() {
+        rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = HabitsAdapter(HabitDbTable(this).read())
     }
 }
--- app/src/main/java/com/amqo/habittrainer/persistence/HabitDbTable.kt
+++ app/src/main/java/com/amqo/habittrainer/persistence/HabitDbTable.kt
@@ -2,10 +2,17 @@ package com.amqo.habittrainer.persistence
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
 import com.amqo.habittrainer.Habit
+import com.amqo.habittrainer.persistence.HabitEntry.DESCR_COL
+import com.amqo.habittrainer.persistence.HabitEntry.IMAGE_COL
+import com.amqo.habittrainer.persistence.HabitEntry.TABLE_NAME
+import com.amqo.habittrainer.persistence.HabitEntry.TITLE_COL
+import com.amqo.habittrainer.persistence.HabitEntry._ID
 import java.io.ByteArrayOutputStream
 
 class HabitDbTable(context: Context) {
@@ -14,23 +21,47 @@ class HabitDbTable(context: Context) {
 
     private val dbHelper = HabitTrainerDb(context)
 
-    fun store(habit:Habit): Long {
+    fun store(habit: Habit): Long {
         val db = dbHelper.writableDatabase
 
         val values = ContentValues()
         with(values) {
-            put(HabitEntry.TITLE_COL, habit.title)
-            put(HabitEntry.DESCR_COL, habit.description)
-            put(HabitEntry.IMAGE_COL, toByteArray(habit.image))
+            put(TITLE_COL, habit.title)
+            put(DESCR_COL, habit.description)
+            put(IMAGE_COL, toByteArray(habit.image))
         }
 
         val id = db.transaction {
-            insert(HabitEntry.TABLE_NAME, null, values)
+            insert(TABLE_NAME, null, values)
         }
-
         Log.d(TAG, "Store new habit to the DB $habit")
 
         return id
+    }
+
+    fun read(): List<Habit> {
+        val db = dbHelper.readableDatabase
+
+        val columns = arrayOf(TITLE_COL, DESCR_COL, IMAGE_COL)
+        val order = "$_ID ASC"
+
+        val cursor = db.doQuery(TABLE_NAME, columns, orderBy = order)
+
+        return parseHabitsFrom(cursor)
+    }
+
+    private fun parseHabitsFrom(cursor: Cursor): MutableList<Habit> {
+        val habits = mutableListOf<Habit>()
+        with(cursor) {
+            while (moveToNext()) {
+                val title = getString(TITLE_COL)
+                val description = getString(DESCR_COL)
+                val habit = Habit(title, description, getBitmap(IMAGE_COL))
+                habits.add(habit)
+            }
+            close()
+        }
+        return habits
     }
 
     private fun toByteArray(bitmap: Bitmap): ByteArray {
@@ -38,6 +69,21 @@ class HabitDbTable(context: Context) {
         bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
         return stream.toByteArray()
     }
+}
+
+private fun Cursor.getString(columnName: String) = getString(getColumnIndex(columnName))
+
+private fun Cursor.getBitmap(columnName: String): Bitmap {
+    val imageByteArray = getBlob(getColumnIndex(columnName))
+    return BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.size)
+}
+
+private fun SQLiteDatabase.doQuery(
+        table: String, columns: Array<String>, selection: String? = null,
+        selectionArgs: Array<String>? = null, groupBy: String? = null,
+        having: String? = null, orderBy: String? = null): Cursor {
+
+    return query(table, columns, selection, selectionArgs, groupBy, having, orderBy)
 }
 
 // inline here will take this exact code and put it in the place where it was called from
