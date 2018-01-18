47165ef18220283319caae587b5f00ab45ef46fa
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-18 10:40:00 +0100

    Prepare first version of SQLite DB

--- app/src/main/java/com/amqo/habittrainer/persistence/Contracts.kt
+++ app/src/main/java/com/amqo/habittrainer/persistence/Contracts.kt
@@ -7,6 +7,7 @@ val DATABASE_VERSION = 10
 
 object HabitEntry: BaseColumns {
     val TABLE_NAME = "habit"
+    val _ID = "id"
     val TITLE_COL = "title"
     val DESCR_COL = "description"
     val IMAGE_COL = "image"
